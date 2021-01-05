e063a3e73eb5cb86d9abd6595df3f4696ad99006
Paola Garay <garay.angela@gmail.com>
2021-01-04 19:53:05 -0500

    Add React Client and setup Apollo Client

---
+++ client/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/styles/index.css';
+import App from './components/App';
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
