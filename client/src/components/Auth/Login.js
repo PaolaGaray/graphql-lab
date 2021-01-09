c33d8eb6cdd94569600e27fc877eb04491a8b972
Paola Garay <garay.angela@gmail.com>
2021-01-08 19:51:17 -0500

    Code cleanup

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -3,38 +3,41 @@ import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 
 import Context from '../../context';
-
-const ME_QUERY = `
-    {
-        me {
-        _id
-        name
-        email
-        picture
-        }
-    }
-  `
+import { ME_QUERY } from '../../graphql/queries';
 
 export default function Login() {
 
     const { dispatch } = useContext(Context);
 
     const onSuccess = async googleUser => {
-        const tokenId = googleUser.getAuthResponse().id_token;
-        const client = new GraphQLClient('http://localhost:4000/graphql', {
-            headers: { authorization: tokenId }
-        })
-        const data = await client.request(ME_QUERY);
-        //console.log({ data });
-        dispatch({ type: "LOGIN_USER", payload: data.me })
-      };
+        try {
+            const tokenId = googleUser.getAuthResponse().id_token;
+            const client = new GraphQLClient('http://localhost:4000/graphql', {
+                headers: { authorization: tokenId }
+            });
+            const { me } = await client.request(ME_QUERY);
+            dispatch({ type: "LOGIN_USER", payload: me })
+        } catch (err) {
+            onFailure(err);
+        }
+    }
+
+    const onFailure = err => {
+        console.error("Error logging in", err)
+    }
 
 
     return (
-        <GoogleLogin
-            clientId="199645287050-2eq6u7n8us9088gn79glhqsmnrjoftkn.apps.googleusercontent.com"
-            onSuccess={onSuccess}
-            isSignedIn={true}
-        />
+        <div>
+        <h1>Welcome</h1>
+             <GoogleLogin
+                clientId="199645287050-2eq6u7n8us9088gn79glhqsmnrjoftkn.apps.googleusercontent.com"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                isSignedIn={true}
+                theme="dark"
+            />
+        </div>
+
     )
 }
